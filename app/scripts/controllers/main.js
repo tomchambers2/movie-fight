08682b2bb52bde8915f059fa0f6a01d521c04f8c
Tom Chambers <tom.chambers@gmail.com>
2014-08-30 23:58:35 +0100

    added analytics

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -20,6 +20,16 @@ angular.module('moviefightApp')
   	};
 
   	$scope.search = function() {
+  		mixpanel.track(
+  			'search',
+  			{ 'Movie': $scope.query }
+  		);
+
+  		calq.action.track(
+		    'search',
+		    { 'Movie': $scope.query }
+		);
+
   		$scope.loading = true;
 
   		Imdbapi.doSearch($scope.query,$scope.year).then(function(result) {
@@ -27,21 +37,26 @@ angular.module('moviefightApp')
 	  			$scope.movies.push(result.data);
 	  			$scope.query = '';
 	  			$scope.year = '';
+	  			mixpanel.track('search-success');
 	  		} else {
 	  			var message = 'Sorry, we couldn\'t find that movie';
 	  			if (!$scope.year) {
 	  				message += '. Try adding a year?';
 	  			}
 	  			showMessage(message);
+	  			mixpanel.track('search-not-found', { 'Movie': $scope.query });
+	  			calq.action.track('search-not-found', { 'Movie': $scope.query });
 	  		}
 	  		$scope.loading = false;	  		
   		}, function() {
   			showMessage('Sorry, the service may not be working at the moment. Try us later.');
+  			mixpanel.track('http-fail');
   			$scope.loading = false;
   		});
   	};
 
   	$scope.remove = function(id) {
   		$scope.movies.splice(id,1);
+  		mixpanel.track('removed-movie');
   	};
   });
